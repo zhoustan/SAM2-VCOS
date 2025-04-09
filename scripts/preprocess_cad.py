f89ec25603dc69410e2ff556e6233b7c747aff2b
Yuli Zhou <42245680+zhoustan@users.noreply.github.com>
2025-04-10 00:08:10 +0200

    Update preprocess_cad.py

--- scripts/preprocess_cad.py
+++ scripts/preprocess_cad.py
@@ -8,15 +8,15 @@ classes = os.listdir(test_data_folder)
 classes = [cls for cls in classes if cls[-4:] != ".txt" and cls[-4:] != "json" and cls[0] != '.']
 for cls in classes:
     # video_dir = test_data_folder + cls + '/frames/'
-    gt_dir = gt_dir + cls + '/groundtruth/'
+    new_gt_dir = gt_dir + cls + '/groundtruth/'
     gt_files = [
-        p for p in os.listdir(gt_dir)
+        p for p in os.listdir(new_gt_dir)
         if os.path.splitext(p)[-1] in [".jpg", ".jpeg", ".JPG", ".JPEG", ".png", ".PNG"]
     ]
 
     for old_name in gt_files:
         new_name = old_name.split('_')[0]+'.png'
-        os.system("mv %s %s "%(os.path.join(gt_dir, old_name), os.path.join(gt_dir, new_name)))
+        os.system("mv %s %s "%(os.path.join(new_gt_dir, old_name), os.path.join(new_gt_dir, new_name)))
 
     video_dir = test_data_folder + cls + '/frames/'
     new_video_dir = test_data_folder + cls + '/new_frames'
